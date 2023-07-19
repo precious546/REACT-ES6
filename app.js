8bc68f5733d4693a9c6154c126fa1d7b664a1d67
precious <preciousbamidele9@gmail.com>
2023-07-19 13:17:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//ES6
+let VariableName = 'VariableValue';
+VariableName = {};
+
+
+
+const personName = 'Kenny';
+//ES5 CONCATENATING 
+console.log('The name of this person is ' + personName);
+//ES6 CONCATENATING
+console.log(`The name of this person is ${personName}`)
